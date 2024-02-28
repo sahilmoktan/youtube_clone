c9873254a2747450328bdba974e9992bc181961a
sahilmoktan <moktanshail9@gmail.com>
2024-02-28 23:57:31 +0545

    able to create headers

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -2,9 +2,14 @@ import {Bell, Menu, Mic, Search, Upload, User} from "lucide-react"
 import logo from "../assets/R.png"
 import { Button } from "../components/Button"
 
+
 const PageHeader = () => {
+
   return (
     <div className='flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4'>
+
+        
+
         <div className='flex gap-4 items-center flex-shrink-0'>
             <Button variant="ghost" size="icon">
                 <Menu/>
@@ -14,7 +19,39 @@ const PageHeader = () => {
             </a>
         </div>
         
-        
+        <form className="md:flex hidden gap-4 flex-grow  justify-center">
+            
+            <div className="flex flex-grow max-w-[600px]">
+                <input type="search" placeholder="Yoyo Search" className="rounded-l-full border border-secondary-border shadow-inner shadow-secondary py-1 px-4 text-lg w-full focus:border-blue-500 outline-none" />
+                <Button className="py-2 px-4 rounded-r-full border-secondary-border border border-l-0 flex-shrink-0">
+                    <Search/>
+                </Button>
+            </div>
+            
+            <Button type="button" size='icon'>
+            <Mic/>
+            </Button>
+        </form>
+
+        <div className="flex flex-shrink-0 md:gap-2">
+            <Button size="icon" variant="ghost" className="md:hidden">
+                <Search/>
+            </Button>
+            <Button size="icon" variant="ghost" className="md:hidden">
+                <Mic/>
+            </Button>
+            <Button size="icon" variant="ghost">
+                <Upload/>
+            </Button>
+            <Button size="icon" variant="ghost">
+                <Bell/>
+            </Button>
+            <Button size="icon" variant="ghost">
+                <User/>
+            </Button>
+            
+
+        </div>
     </div>
   )
 }
