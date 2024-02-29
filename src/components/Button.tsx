ffca3e1e2a61eaafb7fbf54f547fad0318eb26b1
sahilmoktan <moktanshail9@gmail.com>
2024-02-29 23:40:45 +0545

    search bar update for small screen

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ export const buttonStyles = cva(["transition-colors"], {
     variants: {
         variant: {
             default: ["bg-secondary", "hover:bg-secondary-hover"],
-            ghost: ["hover:bg-gray-100"],
+            ghost: ["hover:bg-gray-300"],
             dark: ["bg-secondary-dark", "hover:bg-secondary-dark-hover",
                 "text-secondary"
             ]
--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,16 +1,18 @@
-import {Bell, Menu, Mic, Search, Upload, User} from "lucide-react"
+import {ArrowLeft, Bell, Menu, Mic, Search, Upload, User} from "lucide-react"
 import logo from "../assets/R.png"
 import { Button } from "../components/Button"
+import { useState } from "react";
+
 
 
 const PageHeader = () => {
+    const [showFullWidthSearch, setShowFullWidthSearch] = useState(false);
 
   return (
     <div className='flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4'>
-
         
 
-        <div className='flex gap-4 items-center flex-shrink-0'>
+        <div className={`flex gap-4 items-center flex-shrink-0 ${showFullWidthSearch ? "hidden" : "flex"}`}>
             <Button variant="ghost" size="icon">
                 <Menu/>
             </Button>
@@ -19,22 +21,34 @@ const PageHeader = () => {
             </a>
         </div>
         
-        <form className="md:flex hidden gap-4 flex-grow  justify-center">
-            
-            <div className="flex flex-grow max-w-[600px]">
-                <input type="search" placeholder="Yoyo Search" className="rounded-l-full border border-secondary-border shadow-inner shadow-secondary py-1 px-4 text-lg w-full focus:border-blue-500 outline-none" />
-                <Button className="py-2 px-4 rounded-r-full border-secondary-border border border-l-0 flex-shrink-0">
-                    <Search/>
-                </Button>
-            </div>
-            
-            <Button type="button" size='icon'>
-            <Mic/>
-            </Button>
-        </form>
+        <form className={` gap-4 flex-grow justify-center ${showFullWidthSearch ? "flex" : "hidden md:flex"}`}>
+                {showFullWidthSearch && (
+                    <Button
+                        onClick={() => setShowFullWidthSearch(false)}
+                        type="button" size="icon" variant="ghost" className="flex-shrink-0">
+                        <ArrowLeft />
+                    </Button>
+                )}
 
-        <div className="flex flex-shrink-0 md:gap-2">
-            <Button size="icon" variant="ghost" className="md:hidden">
+
+                <div className="flex flex-grow max-w-[600px]">
+                    <input
+                        type="search"
+                        placeholder="Search"
+                        className="rounded-l-full border border-secondary-border shadow-inner shadow-secondary py-1 px-4 text-lg w-full focus:border-blue-500 outline-none"
+                    />
+                    <Button className="py-2 px-4 rounded-r-full border-secondary-border border border-l-0 flex-shrink-0">
+                        <Search />
+                    </Button>
+                </div>
+
+                <Button type="button" size="icon" className="flex-shrink-0">
+                    <Mic />
+                </Button>
+            </form>
+
+        <div className={`flex-shrink-0 md:gap-2 ${showFullWidthSearch ? "hidden" : "flex"}`}>
+            <Button onClick={() => setShowFullWidthSearch(true)} size="icon" variant="ghost" className="md:hidden">
                 <Search/>
             </Button>
             <Button size="icon" variant="ghost" className="md:hidden">
@@ -57,3 +71,4 @@ const PageHeader = () => {
 }
 
 export default PageHeader
+
