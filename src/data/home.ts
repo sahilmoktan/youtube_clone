22a0acc83f59ffc9ff5b690d4770aacdb8f431e2
sahilmoktan <moktanshail9@gmail.com>
2024-03-01 23:58:28 +0545

    button list

---
+++ src/data/home.ts
@@ -0,0 +1,18 @@
+export const categories = [
+    "All",
+    "JavaScript",
+    "TypeScript",
+    "Programming",
+    "Travelling",
+    "Cricket World Cup 2023",
+    "Bowling",
+    "Cricket",
+    "React",
+    "Next.js",
+    "UI/UX Design",
+    "Object Oriented Programming",
+    "Frontend Web Development",
+    "Backend Web Development",
+    "Web Development",
+    "Coding",
+]
