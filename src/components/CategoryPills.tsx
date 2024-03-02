3c638f24224a13c96e3e12a6ced39cfd7160d0b0
sahilmoktan <moktanshail9@gmail.com>
2024-03-02 23:35:53 +0545

    catagory slider update

--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -56,7 +56,44 @@ export function CategoryPills({ categories, selectedCategory, onSelect }: Catego
 
             </div>
 
-            
+            {isLeftVisible && (
+                <div className="absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r from-white from-50% to-transparent w-24 h-full">
+                    <Button variant="ghost" size="icon" className="h-full aspect-square w-auto p-1.5"
+                        onClick={() => {
+                            setTranslate(translate => {
+                                const newTranslate = translate - TRANSLATE_AMOUNT
+                                if (newTranslate <= 0) return 0
+                                return newTranslate
+                            })
+                        }}
+                    >
+                        <ChevronLeft />
+                    </Button>
+                </div>
+            )}
+
+            {isRightVisible && (
+                <div className="absolute right-0 top-1/2 -translate-y-1/2 bg-gradient-to-l from-white from-50% to-transparent w-24 h-full flex justify-end">
+                    <Button variant="ghost" size="icon" className="h-full aspect-square w-auto p-1.5"
+                        onClick={() => {
+                            setTranslate(translate => {
+                                if (containerRef.current == null) {
+                                    return translate
+                                }
+                                const newTranslate = translate + TRANSLATE_AMOUNT
+                                const edge = containerRef.current.scrollWidth
+                                const width = containerRef.current.clientWidth
+
+                                if (newTranslate + width >= edge) return edge - width
+                                return newTranslate
+                            })
+                        }}
+
+                    >
+                        <ChevronRight />
+                    </Button>
+                </div>
+            )}
 
 
         </div>
