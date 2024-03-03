43c2a34adfd01552ba2aded9b460547945a4178c
sahilmoktan <moktanshail9@gmail.com>
2024-03-03 18:43:01 +0545

    able to show thumbnails

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,38 @@
 // import { useState } from 'react'
-import { useState } from 'react'
-import { CategoryPills } from './components/CategoryPills'
-import { categories } from './data/home'
-import PageHeader from './layouts/PageHeader'
-import VideoGridItem from './components/VideoGridItem'
-
+import { useState } from "react";
+import { CategoryPills } from "./components/CategoryPills";
+import { categories, videos } from "./data/home";
+import PageHeader from "./layouts/PageHeader";
+import { VideoGridItem } from "./components/VideoGridItem";
 
 function App() {
-  const [selectedCategory, setSelectedCategory] = useState(categories[0])
-  
+  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
 
   return (
     <>
-      <div className='max-h-screen flex flex-col'>
-        <PageHeader/>
-        <div className='grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto'>
+      <div className="max-h-screen flex flex-col">
+        <PageHeader />
+        <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
           <div>sidebar</div>
-          <div className='overflow-x-hidden px-8 pb-4'>
-          <div className="sticky top-0 bg-white z-10 pb-4">
-          <CategoryPills categories={categories}
-          selectedCategory={selectedCategory}
-          onSelect={setSelectedCategory}/>
+          <div className="overflow-x-hidden px-8 pb-4">
+            <div className="sticky top-0 bg-white z-10 pb-4">
+              <CategoryPills
+                categories={categories}
+                selectedCategory={selectedCategory}
+                onSelect={setSelectedCategory}
+              />
+            </div>
+
+            <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
+              {videos.map((video) => (
+                <VideoGridItem key={video.id} {...video} />
+              ))}
+            </div>
           </div>
-          
-          <div className='grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]'>
-                  <VideoGridItem />
-
-              </div>
-
-          </div>
-
-          
-          </div>  
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/VideoGridItem.tsx
+++ src/components/VideoGridItem.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
 
-const VideoGridItem = () => {
-  return (
-    <div>VideoGridItem</div>
-  )
+
+
+
+type VideoGridItemProps = {
+  id: string;
+  title: string;
+  channel: {
+    id: string;
+    name: string;
+    profileUrl: string;
+  };
+  views: number;
+  postedAt: Date;
+  duration: number;
+  thumbnailUrl: string;
+  videoUrl: string;
+};
+
+export function VideoGridItem({
+  id,
+  title,
+  channel,
+  views,
+  postedAt,
+  duration,
+  thumbnailUrl,
+  videoUrl,
+}: VideoGridItemProps) {
+  
+  
+    return (
+    <div className="flex flex-col gap-2">
+      <a href={`/watch?v=${id}`} className="relative aspect-video">
+      <img src={thumbnailUrl} className='block w-full h-full object-cover rounded-xl' />
+
+      <div className="absolute bottom-1 right-1 bg-secondary-dark text-secondary text-sm px-0.5 rounded">
+        {duration}
+      </div>
+      </a>
+    </div>
+  );
 }
-
-export default VideoGridItem
